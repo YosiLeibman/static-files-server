fe17e9d680d4ba23103823fe633885f1647762c1
YosiLeibman <yosil.770@gmail.com>
2020-03-19 10:08:22 +0200

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 let express = require('express')
 let app = express()
 let path = require('path')
+let fs = require('fs')
+let formidable = require('formidable')
 
 app.use(express.static('public'))
+
+app.post('/', (req, res) => {
+	const form = new formidable.IncomingForm()
+
+	form.parse(req, (err, fields, files) => {
+		let oldpath = files.filetoupload.path
+		let newpath = path.join(__dirname + "/public/"+  files.filetoupload.name)
+		fs.rename(oldpath, newpath, function(err) {
+			if (err) throw err
+			res.send('File uploaded and saved!')
+		})
+	})
+})
 
 // app.get('/', (req, res) => {
 // 	res.sendFile(path.join(__dirname + '/index.html'))
